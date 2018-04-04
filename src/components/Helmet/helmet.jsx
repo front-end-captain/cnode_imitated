503deccad15611ed8aa485903340c272b21253a5
front-end-captain <18810940932@163.com>
2018-04-05 00:32:30 +0800

    修改导入编辑器样式代码的位置

--- src/components/Helmet/helmet.jsx
+++ src/components/Helmet/helmet.jsx
@@ -6,7 +6,6 @@ const HelmetSection = () => {
 		<Helmet>
 			<meta name="description" content="this is description" />
 			<meta name="description" content="this is another description" />
-			<link rel="stylesheet" href="https://cdn.bootcss.com/simplemde/1.11.2/simplemde.min.css"/>
 			<title>cnode</title>
 		</Helmet>
 	);
